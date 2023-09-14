4b4f1e40c52956f20a45d1534ad0733b0b3311f3
esraguuler <65496194+esraguuler@users.noreply.github.com>
2023-09-15 00:31:05 +0300

    EntityFramework

---
+++ EntityFramework/EntityFramework/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace EntityFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnListele = new System.Windows.Forms.Button();
+            this.BtnEkle = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.TxtPersonelID = new System.Windows.Forms.TextBox();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.TxtSehir = new System.Windows.Forms.TextBox();
+            this.TxtMaas = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtYas = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TxtDurum = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(55, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Personel Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(55, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(55, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Soyad:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(55, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Şehir:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(55, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Maaş:";
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnListele.Location = new System.Drawing.Point(373, 12);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(118, 42);
+            this.BtnListele.TabIndex = 5;
+            this.BtnListele.Text = "Listele";
+            this.BtnListele.UseVisualStyleBackColor = true;
+            // 
+            // BtnEkle
+            // 
+            this.BtnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnEkle.Location = new System.Drawing.Point(373, 75);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(118, 40);
+            this.BtnEkle.TabIndex = 6;
+            this.BtnEkle.Text = "Ekle";
+            this.BtnEkle.UseVisualStyleBackColor = true;
+            this.BtnEkle.Click += new System.EventHandler(this.BtnEkle_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnSil.Location = new System.Drawing.Point(373, 138);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(118, 40);
+            this.BtnSil.TabIndex = 7;
+            this.BtnSil.Text = "Sil";
+            this.BtnSil.UseVisualStyleBackColor = true;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.Location = new System.Drawing.Point(373, 204);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(118, 44);
+            this.BtnGuncelle.TabIndex = 8;
+            this.BtnGuncelle.Text = "Güncelle";
+            this.BtnGuncelle.UseVisualStyleBackColor = true;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // TxtPersonelID
+            // 
+            this.TxtPersonelID.Location = new System.Drawing.Point(154, 30);
+            this.TxtPersonelID.Name = "TxtPersonelID";
+            this.TxtPersonelID.Size = new System.Drawing.Size(139, 20);
+            this.TxtPersonelID.TabIndex = 9;
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(154, 61);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(139, 20);
+            this.TxtAd.TabIndex = 10;
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(154, 93);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(139, 20);
+            this.TxtSoyad.TabIndex = 11;
+            // 
+            // TxtSehir
+            // 
+            this.TxtSehir.Location = new System.Drawing.Point(154, 126);
+            this.TxtSehir.Name = "TxtSehir";
+            this.TxtSehir.Size = new System.Drawing.Size(139, 20);
+            this.TxtSehir.TabIndex = 12;
+            // 
+            // TxtMaas
+            // 
+            this.TxtMaas.Location = new System.Drawing.Point(154, 162);
+            this.TxtMaas.Name = "TxtMaas";
+            this.TxtMaas.Size = new System.Drawing.Size(139, 20);
+            this.TxtMaas.TabIndex = 13;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 303);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 195);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(55, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Yaş:";
+            // 
+            // TxtYas
+            // 
+            this.TxtYas.Location = new System.Drawing.Point(154, 231);
+            this.TxtYas.Name = "TxtYas";
+            this.TxtYas.Size = new System.Drawing.Size(139, 20);
+            this.TxtYas.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(55, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Durum:";
+            // 
+            // TxtDurum
+            // 
+            this.TxtDurum.Location = new System.Drawing.Point(154, 200);
+            this.TxtDurum.Name = "TxtDurum";
+            this.TxtDurum.Size = new System.Drawing.Size(139, 20);
+            this.TxtDurum.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 527);
+            this.Controls.Add(this.TxtDurum);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.TxtYas);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.TxtMaas);
+            this.Controls.Add(this.TxtSehir);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.TxtPersonelID);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.BtnEkle);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnListele;
+        private System.Windows.Forms.Button BtnEkle;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnGuncelle;
+        private System.Windows.Forms.TextBox TxtPersonelID;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.TextBox TxtSehir;
+        private System.Windows.Forms.TextBox TxtMaas;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtYas;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox TxtDurum;
+    }
+}
+
---
+++ EntityFramework/EntityFramework/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        PersonelEntities ent = new PersonelEntities();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ent.PerTable.ToList();
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            PerTable perTable = new PerTable();
+            perTable.PerAd = TxtAd.Text;
+            perTable.PerSoyad = TxtSoyad.Text;
+            perTable.PerSehir = TxtSehir.Text;
+            perTable.PerMaas = Convert.ToInt16(TxtMaas.Text);
+            perTable.PerDurum = Convert.ToBoolean (TxtDurum.Text);
+            perTable.PerYas = TxtYas.Text;
+            ent.PerTable.Add(perTable);
+            ent.SaveChanges();
+            MessageBox.Show("Personel Sisteme Kaydedildi");
+            dataGridView1.DataSource = ent.PerTable.ToList();
+                
+
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt16(TxtPersonelID.Text);
+
+            PerTable perTable = ent.PerTable.First(f => f.Perid == id);
+            ent.PerTable.Remove(perTable);
+            ent.SaveChanges();
+            MessageBox.Show("Personel Sistemden Silindi");
+            dataGridView1.DataSource = ent.PerTable.ToList();
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt16(TxtPersonelID.Text);
+
+            PerTable perTable = ent.PerTable.First(f => f.Perid == id);
+            perTable.PerAd = TxtAd.Text;
+            perTable.PerSoyad = TxtSoyad.Text;
+            perTable.PerSehir = TxtSehir.Text;
+            perTable.PerMaas = Convert.ToInt16(TxtMaas.Text);
+            perTable.PerDurum = Convert.ToBoolean(TxtDurum.Text);
+            perTable.PerYas = TxtYas.Text;
+            ent.PerTable.Add(perTable);
+            ent.SaveChanges();
+            MessageBox.Show("Personel Bilgisi Güncellendi");
+            dataGridView1.DataSource = ent.PerTable.ToList();
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+    }
+}
